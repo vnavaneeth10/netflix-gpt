e4960e51a171fc40b5261738b0fd753f31913b1f
Navaneeth V <navaneethnair69@gmail.com>
2024-07-11 12:59:04 +0530

    Completed user authentication in login and logout

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,7 +2,8 @@ import React from 'react'
 import Header from './Header'
 import { useState, useRef } from 'react';
 import {checkValidData} from "../utils/Validate"
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {  createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import {auth} from "../utils/firebase"
 
 
 const Login = () => {
@@ -26,9 +27,32 @@ const Login = () => {
       //sign in sign up Logic
       if(!isSignInForm){
         //signup logic
+        createUserWithEmailAndPassword(auth, email.current.value, password.current.value)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    console.log(user);
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    setErrorMessage(errorCode + "-" + errorMessage)
+  });
+
 
       } else {
         //sign in logic
+        signInWithEmailAndPassword(auth, email.current.value, password.current.value)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log(user)
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    setErrorMessage(errorCode + "-" + errorMessage);
+  });
 
       }
   }
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,4 +22,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-const auth = getAuth();
+export const auth = getAuth();
